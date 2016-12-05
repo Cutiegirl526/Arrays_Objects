15d44587712d146e4470060d98da59d9ab3dd6ee
cutiegirl526 <kayladiniega@gmail.com>
2016-12-05 08:38:24 -1000

    no mo time

---
+++ arrays_objects.js
@@ -0,0 +1,23 @@
+/*
+
+Kayla Diniega
+Arrays and Objects
+Dec. 5, 2016
+
+*/
+
+
+//variables
+//arrays
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zakattak", "Yolo Baggin", "Ohai", "Basic Jeff"];
+var colors = ["light black", "blue", "red"];
+var adj = ["smart", "Special case", "yummy", "tiny whiny", "scared"];
+var slang = ["scrap", "kobe beef", "da kine", "li day", "pau hana"];
+
+//objects
+
+var donut = {
+	topppings : ["spinkle","chocolate frosting", "o'hare air"],
+	fillings : "Custard"
+}
