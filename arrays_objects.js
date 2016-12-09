758bc559f8030ba7b16639e536ad2d84f3630b76
cutiegirl526 <kayladiniega@gmail.com>
2016-12-09 08:14:52 -1000

    I don't know

--- arrays_objects.js
+++ arrays_objects.js
@@ -18,7 +18,7 @@ var slang = ["scrap", "kobe beef", "da kine", "li day", "pau hana"];
 //objects
 
 var doughnut = {
-	topppings : ["spinkle","chocolate frosting", "o'hare air"],
+	toppings : ["spinkle","chocolate frosting", "o'hare air"],
 	fillings : "Custard";
 	shape : "square";
 	count : 12
@@ -26,6 +26,26 @@ var doughnut = {
 
 // Progression part 2
 
-function candyShop (p1){
-	if
+function candyShop(p1) {
+	console.log(p1[9] + x[4]);
 }
+
+candyShop(numbers);
+
+function sleep(p2){
+	for (i = 0; i <= p2.legnth; i++) {
+		console.log("What would you want from" + p2[i]);
+	}
+}
+
+sleep (doughnut.toppings);
+
+function satisfaction(p3) {
+	if (p3 == doughnut.fillings[2]) {
+		console.log("This is so good. Like to die for good!");
+	} else {
+		console.log("This doesn't taste that great. The " + doughnut.toppings + " looks stale and crusty!");
+	}
+}
+
+satisfaction (doughnut.toppings[4]); 
